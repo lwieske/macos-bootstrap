fff5ee78b4563fc0f12dbc636962e87e76dc856c
Lothar Wieske <lothar.wieske@web.de>
2017-01-20 23:52:01 +0100

    Script to update brews and casks

---
+++ latest.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+###############################################################################
+
+# ask for the administrator password upfront
+sudo -v
+
+# keep-alive - update existing `sudo` time stamp until finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+###############################################################################
+
+(set -x; brew update;)
+
+(set -x; brew upgrade;)
+
+(set -x; brew cleanup;)
+(set -x; brew cask cleanup;)
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+casks=( $(brew cask list) )
+
+for cask in ${casks[@]}
+do
+    version=$(brew cask info $cask | sed -n "s/$cask:\ \(.*\)/\1/p")
+    installed=$(find "/usr/local/Caskroom/$cask" -type d -maxdepth 1 -maxdepth 1 -name "$version")
+
+    if [[ -z $installed ]]; then
+        echo "${red}${cask}${reset} requires ${red}update${reset}."
+        (set -x; brew cask uninstall $cask --force;)
+        (set -x; brew cask install $cask --force;)
+    else
+        echo "${red}${cask}${reset} is ${green}up-to-date${reset}."
+    fi
+done
